58cd294a6a9f0718becaaaae3d0b8aac876db077
M7ham6d <M7hamed012@outlook.com>
2026-07-23 19:43:43 +0300

    19.0

--- m7_mcp_ai_connector/wizard/mcp_token_reveal.py
+++ m7_mcp_ai_connector/wizard/mcp_token_reveal.py
@@ -31,8 +31,7 @@ class McpTokenReveal(models.TransientModel):
 
     @api.depends('raw_token')
     def _compute_urls(self):
-        # base = (self.env['ir.config_parameter'].sudo().get_param('web.base.url', '') or '').rstrip('/')
-        base = 'https://kamron-heroic-dorie.ngrok-free.dev'
+        base = (self.env['ir.config_parameter'].sudo().get_param('web.base.url', '') or '').rstrip('/')
         for rec in self:
             token = rec.raw_token or ''
             rec.endpoint_url = base + '/mcp'
